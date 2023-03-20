e2993c59723c152f044c4d7c92590b2f2c6494fc
Jaa171 <127744318+Jaa171@users.noreply.github.com>
2023-03-20 14:21:10 +0200

    Update branch_file.c

--- branch_file.c
+++ branch_file.c
@@ -1,1 +1,40 @@
-asdasdasfffffffffffffffff
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Tree {
+private:
+	string color;
+	int height;
+public:
+
+};
+
+class Human {
+public:
+	string name;
+	int age;
+	string occupation;
+	void Cut(Tree a) {
+		tree.cut_down(a);
+	};
+};
+/*
+class Leaf {
+private:
+	string color;
+	int size;
+public:
+};
+*/
+
+
+int main() {
+	Tree sosna;
+	Human drwal;
+	drwal.name = "Zbyszek\n";
+	drwal.occupation = "woodcutter\n";
+	std::cout << drwal.occupation;
+	 return 0;
+}
