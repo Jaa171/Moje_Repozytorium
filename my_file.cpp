fef8b3940e7e507e50b78b559c3206af1716c093
Jaa171 <127744318+Jaa171@users.noreply.github.com>
2023-04-03 14:23:42 +0300

    Update my_file.cpp

--- my_file.cpp
+++ my_file.cpp
@@ -1,1 +1,55 @@
-bhjdghjvsadcvghjsdcgdhh
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Plant {
+public:
+	virtual void grow(int a);
+
+};
+
+class Soundable {
+public:
+	virtual void makeSound() = 0;
+};
+
+
+class Tree : public Plant,
+		public Soundable
+
+{
+private:
+	string color;
+	int height;
+public:
+	Tree(string color) {
+		this->color = color;
+	}
+	
+	Tree(int height) {
+		this->height = height;
+	
+	}
+	
+
+	void grow(int a) override {
+		height += 3;
+	}
+
+	void makeSound() override {
+		cout << "Szum";
+	}
+};
+
+int main()
+{
+
+	Tree brzoza("bialy");
+	Tree brzoza(5);
+
+	void grow(5);
+	void makeSound();
+
+	//cout << height;
+	//cout << color;
+}
